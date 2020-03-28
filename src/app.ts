5e403d56b650e2bcdbeb61a109b52a1ee42d14aa
Shehab Tweel <shehabtweel@gmail.com>
2020-03-28 23:42:33 +0200

    Fix lint not working

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,27 @@
 export function parse(version: string) {
-    if (typeof(version) !== 'string') {
-        return new Error(`Expected value to be 'string', got '${typeof(version)}' instead.`);
-    }
-    let ver: string = version;
-    ver.split('.');
-    let obj: object = {
-        major: ver[0],
-        minor: ver[1],
-        patch: ver[2]
-    };
-    return obj;
+  if (typeof version !== 'string') {
+    return new Error(`Expected value to be 'string', got '${typeof version}' instead.`);
+  }
+  const ver: string = version;
+  ver.split('.');
+  const obj: object = {
+    major: ver[0],
+    minor: ver[1],
+    patch: ver[2],
+  };
+  return obj;
 }
 
-export function reBuild(version: any) { // If you're going to access a property from a object, it's gonna error. 'any' can fix it!
-    if (!version) {
-        return new Error('Got nothing.');
-    } else if (typeof(version) !== 'object') {
-        return new Error(`Expected value to be \'object\', got '${typeof(version)}' instead.`);
-    }
+export function reBuild(version: any) {
+  if (!version) {
+    return new Error('Got nothing.');
+  } else if (typeof version !== 'object') {
+    return new Error(`Expected value to be \'object\', got '${typeof version}' instead.`);
+  }
 
-    let major: string = version.major;
-    let minor: string = version.minor;
-    let patch: string = version.patch;
+  const major: string = version.major;
+  const minor: string = version.minor;
+  const patch: string = version.patch;
 
-    return `${major}.${minor}.${patch}`;
+  return `${major}.${minor}.${patch}`;
 }
